7966ef31d87354f37600b419724c0ca7ffcc2ada
Ritwik Shaw <pluto@Dotcom.local>
2020-11-07 16:09:15 +0530

    first commit

---
+++ build/precache-manifest.b52047871a5a9790011285cfd7b112ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6ce97e7b7ff472072808ef2dbd2822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "777560c34a6984295d3e",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "c9127dd15f740e25254a",
+    "url": "/static/js/2.4f6a29ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4f6a29ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "777560c34a6984295d3e",
+    "url": "/static/js/main.bba31a8c.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
